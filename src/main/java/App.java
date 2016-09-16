b45fab21a8958a775428e7739237111ffdee6087
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-09-16 09:04:36 -0700

    fix bug that displayed 'add more words' messaging when there were no words.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      //model.put("words", Word.all());
+      model.put("words", Word.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
